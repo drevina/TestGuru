6391d1a2ee010e80bca2654ea93503d61f0e941e
Sasha_Drevina <_@email.com>
2021-06-30 21:12:08 +0300

    Lesson 5: associations and relations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  belongs_to :creator, class_name: "User", foreign_key: "user_id"
+
   def self.sort_tests_by_category(category)
     Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category).order('tests.title DESC').pluck('tests.title')
   end
