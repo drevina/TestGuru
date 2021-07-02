7a4009e4101a818d8ea54e394be3676d30a419f1
Sasha_Drevina <_@email.com>
2021-07-02 22:16:08 +0300

    Lesson 6: scopes and validations

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,16 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
 
-  def self.sort_tests_by_category(category)
-    Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category).order('tests.title DESC').pluck('tests.title')
-  end
+  validates :title, presence: true,
+                    uniqueness: { scope: :level }
+
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+                    uniqueness: { scope: :title }
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :normal, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  #scope-method instead of model-method -->
+  scope :sort_by_category, -> (category) { joins(:category).where(categories.title: category).order(tests.title: :desc) }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_users
   has_many :created_tests, class_name: "Test"
 
-  def user_tests_by_level(level)
-    Test.where(level: level).joins('JOIN tests_users ON tests_users.test_id = tests.id').where(tests_users: {user_id: id})
-  end
+  validates :email, presence: true
+  validates :name, presence: true
+  
+  #scope-method instead of model-method -->
+  scope :user_tests_by_level, -> (level) { where(level: level).joins(:tests_user).where(tests_users: {user_id: id}) }
+
 end
