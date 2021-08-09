b44b19692c79e694313bf410ab2e9f715cbcc4fe
Sasha_Drevina <_@email.com>
2021-08-09 19:45:48 +0300

    Lesson 13: basic bootstrap settings and designs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def github_url(author, repo)
-    link_to "Project", "https://github.com/#{author}/#{repo}", target: '_blank'
+    link_to "Github", "https://github.com/#{author}/#{repo}", target: '_blank'
   end
 
   def current_year
